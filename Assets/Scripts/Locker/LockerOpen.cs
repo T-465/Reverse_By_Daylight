6fe9c0daf87c559e15519fd3068139d33589b216
T-465 <Dunne-T2@ulster.ac.uk>
2024-07-23 15:53:37 +0100

    idk man

--- Assets/Scripts/Locker/LockerOpen.cs
+++ Assets/Scripts/Locker/LockerOpen.cs
@@ -2,23 +2,23 @@ using GLTF.Schema;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Animation = UnityEngine.Animation;
 
 public class LockerOpen : MonoBehaviour
 {
     public PlayerMovement playerMovement;
     public Animator animator;
-
     private void Start()
     {
         animator = GetComponent<Animator>();    
     }
     public IEnumerator OpeningLocker()
     {
-        animator.SetBool("Open", true);
-        Debug.Log("Pls Work");
-        StartCoroutine(playerMovement.Locker());
-
+       animator.SetBool("Open", true);
         
+       Debug.Log("Pls Work");
+       StartCoroutine(playerMovement.Locker());
+       
 
        yield return new WaitForSeconds(3);
        animator.SetBool("Open", false);
